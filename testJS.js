0e5132c0c42ee44adc2ee10691f1623471b09f12
maunaloa <humchin@maunaloabbq.com>
2017-05-18 11:47:04 -0700

    Clone of MaunaLoa website

---
+++ testJS.js
@@ -0,0 +1,10 @@
+//write a program that prints the numbers from 1-100. For multiples of 3 print "Fizz"
+//instead of the number and multiples of five print "Buzz". For numbers which are
+//multiples of both 3 and 5 pring "FizzBuzz".
+
+function fizzBuzz () {
+
+	for (var i=0; i<101; i++) {
+		console.log(i);
+	}
+}
